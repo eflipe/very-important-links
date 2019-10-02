f8c09a42586f3c423625c8d1933c2fde93c1030c
eflipe <felipecabaleiro@gmail.com>
2019-10-02 10:57:41 -0300

    002 - Configuración Media y Static files y Templates

--- vil_app/urls.py
+++ vil_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from vil_app import views
+# from vil_app import views
 
 app_name = 'vil_app'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
 ]
